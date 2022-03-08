4dc2ed5e82950c862f1f49ca2f002eb4a250a977
Ankush Bahekar <ankushmb9@gmail.com>
2022-03-08 17:14:35 +0530

    [Ankush] Add: Check Employee is present or absent

---
+++ EmployeWageProblemUsingOops/EmployeWageProblemUsingOops/EmployeWageProblem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeWageProblemUsingOops
+{
+    class EmployeWageProblem
+    {
+        readonly int IS_PRESENT = 1;
+        public int empCheck;
+
+        public void Attendance()
+        {
+            Random random = new Random();
+            empCheck = random.Next(0, 2);
+            if (empCheck == IS_PRESENT)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+    }
+}
---
+++ EmployeWageProblemUsingOops/EmployeWageProblemUsingOops/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeWageProblemUsingOops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            EmployeWageProblem EmployeeWage = new EmployeWageProblem();
+            EmployeeWage.Attendance();
+            Console.ReadKey();
+        }
+    }
+}
