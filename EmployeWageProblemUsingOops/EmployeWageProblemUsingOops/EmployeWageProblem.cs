69ed816e33517a851a971a399fc3f4198e712ea4
Ankush Bahekar <ankushmb9@gmail.com>
2022-03-08 18:05:49 +0530

    [Ankush] Add: Solving using Switch Case Statement

--- EmployeWageProblemUsingOops/EmployeWageProblemUsingOops/EmployeWageProblem.cs
+++ EmployeWageProblemUsingOops/EmployeWageProblemUsingOops/EmployeWageProblem.cs
@@ -8,31 +8,35 @@ namespace EmployeWageProblemUsingOops
 {
     class EmployeWageProblem
     {
-        public int IS_PRESENT = 1;
+        const int Emp_Full_Time = 1;
+        const int Emp_Part_Time = 2;
         public int Wage_Per_Hours = 20;
-        // public int Full_Day_Hour = 8;
-        readonly int Emp_Full_Time = 2;
-        readonly int Emp_Part_Time = 1;
-        public int empHrs;
+        public int empHrs = 0;
         public int EmployeeWage;
         public void Attendance()
         {
             // Stroing RandomValue
             Random random = new Random();
-            int empCheck = random.Next(0, 2); //checking Random Value in b/w 0 to 2
-            if (empCheck == Emp_Part_Time) //check if 1 == 1 
+            int empCheck = random.Next(0, 3); //checking Random Value in b/w 0 to 3
+            //Switch Case
+            switch (empCheck)
             {
-                Console.WriteLine("Employee is Present"); //Present
-                empHrs = 4; //* by 4
+                case Emp_Full_Time: // For Emp Full Time  =1
+                    empHrs = 8; // * 8
+                    break;
+
+                case Emp_Part_Time: // For Emp Full Time  =2
+                    empHrs = 4; // * 4
+                    break;
+
+                default:
+                    empHrs = 0; //By Default 0
+                    break;
             }
-            else //if Random is 2 
-            {
-                Console.WriteLine("Employee is Absent"); //absent
-                empHrs = 0; // * 0
-            }
-            //calculate 
+
+            // Calculate
             EmployeeWage = empHrs * Wage_Per_Hours;
-            Console.WriteLine("Employee Full Wage is : " + EmployeeWage); //op
+            Console.WriteLine("Employee wage: " + EmployeeWage); // op
         }
     }
 }
