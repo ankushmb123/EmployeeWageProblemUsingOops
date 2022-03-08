36a80a71172123170cf685116e53a5b119d84482
Ankush Bahekar <ankushmb9@gmail.com>
2022-03-08 18:10:42 +0530

    [Ankush] Add: Calculate Wage For a month

--- EmployeWageProblemUsingOops/EmployeWageProblemUsingOops/EmployeWageProblem.cs
+++ EmployeWageProblemUsingOops/EmployeWageProblemUsingOops/EmployeWageProblem.cs
@@ -10,33 +10,37 @@ namespace EmployeWageProblemUsingOops
     {
         const int Emp_Full_Time = 1;
         const int Emp_Part_Time = 2;
-        public int Wage_Per_Hours = 20;
+        public int Working_Days_Per_Month = 20;
+        public int Total_Salary_Month = 0;
+        public int Day;
         public int empHrs = 0;
         public int EmployeeWage;
         public void Attendance()
         {
             // Stroing RandomValue
-            Random random = new Random();
-            int empCheck = random.Next(0, 3); //checking Random Value in b/w 0 to 3
-            //Switch Case
-            switch (empCheck)
+            for (Day = 1; Day <= Working_Days_Per_Month; Day++)
             {
-                case Emp_Full_Time: // For Emp Full Time  =1
-                    empHrs = 8; // * 8
-                    break;
+                Random random = new Random(); //Random Value
+                int empCheck = random.Next(0, 3); //Check Random in b/w 0 to 3
+                switch (empCheck) // switch Case
+                {
+                    case Emp_Full_Time: // For Full Time =1
+                        empHrs = 8; // *8
+                        break;
 
-                case Emp_Part_Time: // For Emp Full Time  =2
-                    empHrs = 4; // * 4
-                    break;
+                    case Emp_Part_Time: //for Part Time = 2
+                        empHrs = 4; //*4
+                        break;
 
-                default:
-                    empHrs = 0; //By Default 0
-                    break;
+                    default:
+                        empHrs = 0; //Default 0
+                        break;
+                }
             }
-
-            // Calculate
-            EmployeeWage = empHrs * Wage_Per_Hours;
-            Console.WriteLine("Employee wage: " + EmployeeWage); // op
+            //calculating 
+            EmployeeWage = empHrs * Working_Days_Per_Month;
+            Total_Salary_Month += EmployeeWage;  // Total_Salary_Month = Total_Salary_Month + EmployeeWage
+            Console.WriteLine("Employee Wage : " + EmployeeWage); // op
         }
     }
 }
