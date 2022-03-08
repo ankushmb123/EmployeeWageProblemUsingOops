51b2feeb324556442f590d943bc9c1c81b81ddd3
Ankush Bahekar <ankushmb9@gmail.com>
2022-03-08 17:51:07 +0530

    [Ankush] Add: Calculate Daily Employee Wage

--- EmployeWageProblemUsingOops/EmployeWageProblemUsingOops/EmployeWageProblem.cs
+++ EmployeWageProblemUsingOops/EmployeWageProblemUsingOops/EmployeWageProblem.cs
@@ -8,17 +8,32 @@ namespace EmployeWageProblemUsingOops
 {
     class EmployeWageProblem
     {
-        readonly int IS_PRESENT = 1;
-        public int empCheck;
-
+        public int IS_PRESENT = 1;
+        public int Wage_Per_Hours = 20;
+        public int Full_Day_Hour = 8;
+        public int EmployeeWage;
         public void Attendance()
         {
+            // Stroing RandomValue
             Random random = new Random();
-            empCheck = random.Next(0, 2);
-            if (empCheck == IS_PRESENT)
-                Console.WriteLine("Employee is Present");
-            else
-                Console.WriteLine("Employee is Absent");
+            int empCheck = random.Next(0, 2); //checking Random Value in b/w 0 to 2
+            if (empCheck == IS_PRESENT) // if  Is_Present = 1
+            {
+                Console.WriteLine("Employee is Present"); //Emp is Present
+                CalculateDailyWage(); //Calculating Emp Full wage in this function
+            }
+            else  //if Is_Present = 0
+            {
+                Console.WriteLine("Employee is Absent"); //Emp is Absent
+                Console.WriteLine("Employee Wage is null"); // if 0 it will be null
+            }
+
+        }
+        //Calculation emp Full Wage
+        public void CalculateDailyWage()
+        {
+            EmployeeWage = Wage_Per_Hours * Full_Day_Hour;  // Empwage = 20*8 
+            Console.WriteLine("Employee Full Wage is : " + EmployeeWage); //op
         }
     }
 }
