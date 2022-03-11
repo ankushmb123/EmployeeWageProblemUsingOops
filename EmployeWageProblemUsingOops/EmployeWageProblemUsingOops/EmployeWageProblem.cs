b8cf06560356489b118df2986c129aecd6b4932e
Ankush Bahekar <ankushmb9@gmail.com>
2022-03-11 17:41:20 +0530

    [Ankush] Add: Ability to Manage Employee Wage for multiple companies

--- EmployeWageProblemUsingOops/EmployeWageProblemUsingOops/EmployeWageProblem.cs
+++ EmployeWageProblemUsingOops/EmployeWageProblemUsingOops/EmployeWageProblem.cs
@@ -6,59 +6,36 @@ using System.Threading.Tasks;
 
 namespace EmployeWageProblemUsingOops
 {
-    class EmployeWageProblem
+    // class For CompanyEmpWage
+    class CompanyEmpWage
     {
-        const int Emp_Full_Time = 1;
-        const int Emp_Part_Time = 2;
-        // public int Count_Work_Day = 20;
-        // public int Emp_Per_Rate_Hour = 20;
-        //public int MAx_Hrs_Or_Day_Month= 100;
-        public int Working_Days_Per_Month = 20;
-        public int Working_Days = 0;
-        public int empHrs = 0;
-        public int EmployeeWage = 0;
-        int totalWage = 0;
-        int totalHours = 0;
-        public void Attendance(string CompanyName, int WagePerHour, int NumWorkingDays, int MaxHoursPerMonth) //
+        //instance variable
+        public readonly string companyName;
+        public readonly int wagePerHour;
+        public readonly int maxWorkingDays;
+        public readonly int maxWorkingHours;
+        // by default wagesPerMonth  0 
+        int wagesPerMonth = 0;
+
+        //constructor for  CompanyEmpWage using multiple parameters
+        public CompanyEmpWage(string companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours)
         {
-            // Stroing RandomValue
-            for (int Day = 1; Day <= Working_Days_Per_Month; Day++)
-            {
-                Random random = new Random(); //Random Value
-
-                int empCheck = random.Next(0, 3); //Check Random in b/w 0 to 3
-                switch (empCheck) // switch Case
-                {
-                    case Emp_Full_Time: // For Full Time =1
-                        empHrs = WagePerHour * 8; // *8
-                        break;
-
-                    case Emp_Part_Time: //for Part Time = 2
-                        empHrs = WagePerHour * 4; //*4
-                        break;
-
-                    default:
-                        empHrs = 0; //Default 0
-                        break;
-                }
-
-                // EmployeeWage += empHrs; // EmployeeWage = EmployeeWage+ empHrs
-                //Console.WriteLine("Day#:" + Working_Days + "Employee Hrs:" + empHrs);
-                //checking total number of hours
-                totalHours += empHrs / 20; //calculate total hours worked
-                totalWage += empHrs; // calculating montly wage
-                if (totalHours >= MaxHoursPerMonth) //maximum total hours 
-                    break;
-            }
-
-            //EmpSalary = EmployeeWage + Emp_Per_Rate_Hour; // EmpSalary = EmployeeWage + Emp_Per_Rate_Hour
-            // Console.WriteLine("Employee Wage : " + EmployeeWage); // op
-            Console.WriteLine();
-            Console.WriteLine("Company Name :" + CompanyName);
-            Console.WriteLine("Number Of Hour Employee  Worked  :" + totalHours);
-            Console.WriteLine("Employee Wage Per Hour :" + WagePerHour);
-            Console.WriteLine("Monthly wage :" + totalWage);
-            Console.WriteLine();
+            // diff b/w global varable and constructor variable
+            this.companyName = companyName;
+            this.wagePerHour = wagePerHour;
+            this.maxWorkingDays = maxWorkingDays;
+            this.maxWorkingHours = maxWorkingHours;
+        }
+        //instance method for setWagesPerMonth
+        public void setWagesPerMonth(int wagesPerMonth)
+        {
+            this.wagesPerMonth = wagesPerMonth;
+        }
+        //instance method for printMonthlyWage
+        public void printMonthlyWage()
+        {
+            //prinitng  Wage Of a company
+            Console.WriteLine($"Wages for the company {companyName} for the month : {wagesPerMonth}");
         }
     }
 }
--- EmployeWageProblemUsingOops/EmployeWageProblemUsingOops/Program.cs
+++ EmployeWageProblemUsingOops/EmployeWageProblemUsingOops/Program.cs
@@ -12,11 +12,14 @@ namespace EmployeWageProblemUsingOops
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
             //Emp is Present Or Absent
-            EmployeWageProblem EmployeeWage = new EmployeWageProblem();
-            EmployeeWage.Attendance("DMart", 20, 2, 10);
-            EmployeeWage.Attendance("BigBazar", 10, 4, 20);
-            EmployeeWage.Attendance("BigBascket", 15, 6, 22);
-
+            // invoke constructor
+            EmployeWageProblem empWageBuilder = new EmployeWageProblem();
+            ///string companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours
+            empWageBuilder.AddCompany("TCS", 20, 2, 10);
+            empWageBuilder.AddCompany("TATA", 20, 2, 10);
+            empWageBuilder.AddCompany("Infoys", 20, 2, 10);
+            empWageBuilder.AddCompany("Google", 20, 2, 10);
+            empWageBuilder.DisplayCompanyWages();
             Console.ReadKey();
         }
     }
